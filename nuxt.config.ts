b7e330db19e58e8559af5ae26ecfa45c52144966
Ethan Freestone <ejfreebie@gmail.com>
2025-09-30 18:01:17 +0100

    feat: Set up styling with Tailwind, and an index page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -7,6 +9,13 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
+    '@nuxt/ui',
+    '@nuxt/fonts',
+  ],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ],
+  },
 })
