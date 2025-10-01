f69a165ea9b7babaae9c232863f79707723d6de8
ethan-freestone <70844858+ethan-freestone@users.noreply.github.com>
2025-10-01 13:14:24 +0100

    style: Neaten list of env vars

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -64,12 +64,22 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
 
 <template>
   <div class="h-screen flex items-center justify-center">
-    // FIXME DO NOT KEEP THESE ON THE LOGIN PAGE, OBVS
+    {/* FIXME DO NOT KEEP THESE ON THE LOGIN PAGE, OBVS */}
     <UPageCard class="mt-3" title="Env vars">
-		  vercelEnv: {{ config.public.vercelEnv }}
-      vercelUrl: {{ config.public.vercelUrl }}
-      vercelBranchUrl: {{ config.public.vercelBranchUrl }}
-      baseUrl: {{ config.public.baseUrl }}
+		<UPageList>
+			<div key="vercelEnv">
+				vercelEnv: {{ config.public.vercelEnv }}
+		    </div>
+			<div key="vercelUrl">
+      			vercelUrl: {{ config.public.vercelUrl }}
+		    </div>
+			<div key="vercelBranchUrl">
+      			vercelBranchUrl: {{ config.public.vercelBranchUrl }}
+		    </div>
+			<div key="baseUrl">
+				baseUrl: {{ config.public.baseUrl }}
+		    </div>
+		</UPageList>
     </UPageCard>
     <UCard class="ml-5 mr-5">
       <UAuthForm
